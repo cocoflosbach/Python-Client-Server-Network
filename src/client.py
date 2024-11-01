9d3cfe3560adbeba0c98603b18530040c17753a1
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2024-11-01 08:41:07 +0100

    Update client.py

--- src/client.py
+++ src/client.py
@@ -10,10 +10,11 @@ PORT = 5050
 FORMAT = "utf-8"
 DISCONNECT_MESSAGE = "!DISCONNECT"
 SERVER = socket.gethostbyname(socket.gethostname())
-ADDR = (SERVER, PORT)
+
 
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ADDR = (SERVER, PORT)
 client.connect(ADDR)
 
 
